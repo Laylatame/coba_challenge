eb05ab3dbf1b132600e6cb4c52cee998cd24d106
laylatame <layla@teloslabs.co>
2022-04-08 17:03:27 -0400

    add validations and methods for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :bigint           not null, primary key
+#  first_name :string
+#  last_name  :string
+#  province   :string
+#  region     :string
+#  segment    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Customer < ApplicationRecord
+
+  validates :first_name, :last_name, :province, :region, :segment, presence: true
+  validates :segment, inclusion: { in: ['Consumer', 'Corporate', 'Home Office', 'Small Business'] }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
