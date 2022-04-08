0652ae05e5b0fddac1c18227953cbc80235f38fb
laylatame <layla@teloslabs.co>
2022-04-08 17:38:54 -0400

    add validations for product attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,4 +13,14 @@
 #  updated_at  :datetime         not null
 #
 class Product < ApplicationRecord
+
+  CATEGORY_TYPES = ['Furniture', 'Office Supplies', 'Technology']
+  CONTAINER_TYPES = ['Wrap Bag', 'Small Box', 'Small Pack', 'Medium Box',
+                     'Large Box', 'Jumbo Box', 'Jumbo Drum']
+
+  validates :category, :subcategory, :name, :container, 
+            :base_margin, :unit_price, presence: true
+  validates :category, inclusion: { in: CATEGORY_TYPES }
+  validates :container, inclusion: { in: CONTAINER_TYPES }
+  validates :base_margin, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
